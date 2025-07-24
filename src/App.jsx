0755ee99cbba95dbf44dce65ef33b75f968adab9
Aman Rajak <rajakaman366@gmail.com>
2025-07-24 19:15:22 +0530

    Initial commit with About component and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import "./App.css";
+import About from "./component/About";
 import Navbar from "./component/Navbar";
-import TextForm from "./component/TextForm";
+// import TextForm from "./component/TextForm";
 
 function App() {
   return (
     <>
       <Navbar name="Telius" />
       <div className="container my-3">
-        <TextForm heading =" Enter your Text Here To Analyze" />
+        {/* <TextForm heading =" Enter your Text Here To Analyze" /> */}
+       <About/>   
       </div>
-      
-    </>
+        </>
   );
 }
 
